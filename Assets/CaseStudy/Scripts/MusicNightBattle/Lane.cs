c8c2beb11ff1065331925fe55eec594c9612cc7d
thien.nguyenminh <thien.nguyenminhgdv@gmail.com>
2023-06-22 22:59:03 +0700

    feat: add note spawn from midi file BUGS: note hit time is not correct need to fix asap

---
+++ Assets/CaseStudy/Scripts/MusicNightBattle/Lane.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Melanchall.DryWetMidi.Interaction;
+using UnityEngine;
+
+namespace CaseStudy.Scripts.MusicNightBattle
+{
+    public class Lane : MonoBehaviour
+    {
+        [SerializeField] private Melanchall.DryWetMidi.MusicTheory.NoteName _noteRestriction;
+        [SerializeField] private KeyCode _input;
+        public GameObject NotePrefab;
+        private List<Note> _notes = new();
+        public List<double> TimeStamps; // in second
+        private int _spawnIndex = 0;
+        private int _inputIndex = 0;
+
+        public void SetTimeStamp(Melanchall.DryWetMidi.Interaction.Note[] noteArray)
+        {
+            var landNotes = noteArray.Where(note => note.NoteName == _noteRestriction);
+            foreach (var note in landNotes)
+            {
+                // note.Time depend on the Midi time so need to convert it back to metric time
+                var metricTimeSpan =
+                    TimeConverter.ConvertTo<MetricTimeSpan>(note.Time, SongManager.Instance.MidiFile.GetTempoMap());
+                TimeStamps.Add((double)metricTimeSpan.Minutes * 60f + metricTimeSpan.Seconds +
+                               (double)metricTimeSpan.Milliseconds / 1000f);
+            }
+        }
+
+        private void Update()
+        {
+            if (_spawnIndex < TimeStamps.Count)
+            {
+                if (SongManager.GetAudioSourceTime() >= TimeStamps[_spawnIndex] - SongManager.Instance.NoteTime)
+                {
+                    var note = Instantiate(NotePrefab, transform).GetComponent<Note>();
+                    note.SetUp(_input);
+                    _notes.Add(note);
+                    note.AssignedTime = (float)TimeStamps[_spawnIndex];
+                    _spawnIndex++;
+                }
+            }
+
+            if (_inputIndex < TimeStamps.Count)
+            {
+                double timeStamp = TimeStamps[_inputIndex];
+                double marginOfError = SongManager.Instance.MarginOfError;
+                double audioTime = SongManager.GetAudioSourceTime() -
+                                   (SongManager.Instance.InputDelayInMilliseconds / 1000.0);
+
+                if (Input.GetKeyDown(_input))
+                {
+                    if (Math.Abs(audioTime - timeStamp) < marginOfError)
+                    {
+                        Hit();
+                        Debug.Log($"Hit on {_inputIndex} note");
+                        Destroy(_notes[_inputIndex].gameObject);
+                        _inputIndex++;
+                    }
+                    else
+                    {
+                        Debug.Log($"Hit inaccurate on {_inputIndex} note with {Math.Abs(audioTime - timeStamp)} delay");
+                    }
+                }
+
+                if (timeStamp + marginOfError <= audioTime)
+                {
+                    Miss();
+                    Debug.Log($"Missed {_inputIndex} note");
+                    _inputIndex++;
+                }
+            }
+        }
+
+        private void Miss()
+        {
+            // miss note implementation
+        }
+
+        private void Hit()
+        {
+            // hit note implementation
+        }
+    }
+}
---
+++ Assets/CaseStudy/Scripts/MusicNightBattle/SongManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CaseStudy.DesignPattern;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace CaseStudy.Scripts.MusicNightBattle
+{
+    public class SongManager : MonoSingleton<SongManager>
+    {
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private float _songDelayInSeconds;
+        [SerializeField] private int _inputDelayInMilliseconds;
+        [SerializeField] private float _marginOfError;
+
+        [SerializeField] private string _filePath;
+        [SerializeField] private float _noteSpawnY;
+        [SerializeField] private float _noteTapY;
+
+        [SerializeField] private List<Lane> _lanes;
+        public float NoteTime;
+
+        public float NoteDespawnY => _noteTapY - (_noteSpawnY - _noteTapY);
+        public float MarginOfError => _marginOfError;
+        public int InputDelayInMilliseconds => _inputDelayInMilliseconds;
+        [SerializeField] private MidiFile _midiFile;
+        public MidiFile MidiFile => _midiFile;
+        public double debugAudioSourceTime;
+
+        private void Start()
+        {
+            ReadFromFile();
+        }
+
+        private void Update()
+        {
+            debugAudioSourceTime = GetAudioSourceTime();
+        }
+
+        void ReadFromFile()
+        {
+            _midiFile = _midiFile = MidiFile.Read(Application.streamingAssetsPath + "/" + _filePath);
+            GetDataFromMidi();
+        }
+
+        void GetDataFromMidi()
+        {
+            var notes = _midiFile.GetNotes();
+            var noteArray = new Melanchall.DryWetMidi.Interaction.Note[notes.Count];
+            notes.CopyTo(noteArray, 0);
+            foreach (var lane in _lanes)
+            {
+                lane.SetTimeStamp(noteArray);
+            }
+
+            PlaySongDelay(_songDelayInSeconds);
+        }
+
+        // async void PlaySongDelay(float delayInSeconds)
+        // {
+        //     await Task.Delay((int)(delayInSeconds * 1000));
+        // }
+
+        IEnumerator PlaySongDelay(float delayInSeconds)
+        {
+            yield return new WaitForSeconds(delayInSeconds);
+            // playsong
+            _audioSource.Play();
+        }
+
+        public static double GetAudioSourceTime()
+        {
+            return (double)Instance._audioSource.timeSamples / Instance._audioSource.clip.frequency;
+        }
+    }
+}
